ba76d7b3495fc6f6f8920aac2e1516eb03331479
Thibaut Marmin <marminthibaut@gmail.com>
2016-01-05 16:39:12 +0100

    godep save

---
+++ Godeps/_workspace/src/github.com/elastic/beats/libbeat/publisher/async.go
@@ -0,0 +1,93 @@
+package publisher
+
+import (
+	"time"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/elastic/beats/libbeat/outputs"
+)
+
+type asyncPublisher struct {
+	messageWorker
+	outputs []worker
+	pub     *PublisherType
+	ws      workerSignal
+}
+
+const (
+	defaultFlushInterval = 1000 * time.Millisecond // 1s
+	defaultBulkSize      = 200
+)
+
+func newAsyncPublisher(pub *PublisherType) *asyncPublisher {
+
+	p := &asyncPublisher{pub: pub}
+	p.ws.Init()
+
+	var outputs []worker
+	for _, out := range pub.Output {
+		outputs = append(outputs, asyncOutputer(&p.ws, out))
+	}
+
+	p.outputs = outputs
+	p.messageWorker.init(&pub.wsPublisher, defaultChanSize, newPreprocessor(pub, p))
+	return p
+}
+
+// onStop will send stop signal to message batching workers
+func (p *asyncPublisher) onStop() { p.ws.stop() }
+
+func (p *asyncPublisher) onMessage(m message) {
+	debug("async forward to outputers (%v)", len(p.outputs))
+
+	// m.signal is not set yet. But a async client type supporting signals might
+	// be implemented in the furute.
+	// If m.signal is nil, NewSplitSignaler will return nil -> signaler will
+	// only set if client did send one
+	if m.context.signal != nil && len(p.outputs) > 1 {
+		m.context.signal = outputs.NewSplitSignaler(m.context.signal, len(p.outputs))
+	}
+	for _, o := range p.outputs {
+		o.send(m)
+	}
+}
+
+func (p *asyncPublisher) client() eventPublisher {
+	return p
+}
+
+func (p *asyncPublisher) PublishEvent(ctx context, event common.MapStr) bool {
+	p.send(message{context: ctx, event: event})
+	return true
+}
+
+func (p *asyncPublisher) PublishEvents(ctx context, events []common.MapStr) bool {
+	p.send(message{context: ctx, events: events})
+	return true
+}
+
+func asyncOutputer(ws *workerSignal, worker *outputWorker) worker {
+	config := worker.config
+
+	flushInterval := defaultFlushInterval
+	if config.FlushInterval != nil {
+		flushInterval = time.Duration(*config.FlushInterval) * time.Millisecond
+	}
+	logp.Info("Flush Interval set to: %v", flushInterval)
+
+	maxBulkSize := defaultBulkSize
+	if config.BulkMaxSize != nil {
+		maxBulkSize = *config.BulkMaxSize
+	}
+	logp.Info("Max Bulk Size set to: %v", maxBulkSize)
+
+	// batching disabled
+	if flushInterval <= 0 || maxBulkSize <= 0 {
+		return worker
+	}
+
+	debug("create bulk processing worker (interval=%v, bulk size=%v)",
+		flushInterval, maxBulkSize)
+	return newBulkWorker(ws, defaultChanSize, worker, flushInterval, maxBulkSize)
+}
---
+++ Godeps/_workspace/src/github.com/elastic/beats/libbeat/publisher/worker.go
@@ -0,0 +1,100 @@
+package publisher
+
+import (
+	"expvar"
+	"sync"
+
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/outputs"
+)
+
+// Metrics that can retrieved through the expvar web interface.
+var (
+	messagesInWorkerQueues = expvar.NewInt("libbeatMessagesInWorkerQueues")
+)
+
+type worker interface {
+	send(m message)
+}
+
+type messageWorker struct {
+	queue   chan message
+	ws      *workerSignal
+	handler messageHandler
+}
+
+type message struct {
+	context context
+	event   common.MapStr
+	events  []common.MapStr
+}
+
+type workerSignal struct {
+	done chan struct{}
+	wg   sync.WaitGroup
+}
+
+type messageHandler interface {
+	onMessage(m message)
+	onStop()
+}
+
+func newMessageWorker(ws *workerSignal, hwm int, h messageHandler) *messageWorker {
+	p := &messageWorker{}
+	p.init(ws, hwm, h)
+	return p
+}
+
+func (p *messageWorker) init(ws *workerSignal, hwm int, h messageHandler) {
+	p.queue = make(chan message, hwm)
+	p.ws = ws
+	p.handler = h
+	ws.wg.Add(1)
+	go p.run()
+}
+
+func (p *messageWorker) run() {
+	defer p.shutdown()
+	for {
+		select {
+		case <-p.ws.done:
+			return
+		case m := <-p.queue:
+			messagesInWorkerQueues.Add(-1)
+			p.handler.onMessage(m)
+		}
+	}
+}
+
+func (p *messageWorker) shutdown() {
+	p.handler.onStop()
+	stopQueue(p.queue)
+	p.ws.wg.Done()
+}
+
+func (p *messageWorker) send(m message) {
+	p.queue <- m
+	messagesInWorkerQueues.Add(1)
+}
+
+func (ws *workerSignal) stop() {
+	close(ws.done)
+	ws.wg.Wait()
+}
+
+func newWorkerSignal() *workerSignal {
+	w := &workerSignal{}
+	w.Init()
+	return w
+}
+
+func (ws *workerSignal) Init() {
+	ws.done = make(chan struct{})
+}
+
+func stopQueue(qu chan message) {
+	close(qu)
+	for msg := range qu { // clear queue and send fail signal
+		outputs.SignalFailed(msg.context.signal, nil)
+	}
+}
