4add2d34cd87aec863de11debd187cb7ebcc69f5
Thibaut Marmin <marminthibaut@gmail.com>
2016-01-12 14:47:32 +0100

    add some test for dockerbeat

--- beat/Dockerbeat_test.go
+++ beat/Dockerbeat_test.go
@@ -4,28 +4,59 @@ import (
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/publisher"
 )
 
-// helper method
-func getEmptyDockerbeat() Dockerbeat {
-	return Dockerbeat{
-		done:   make(chan struct{}),
-		period: time.Duration(10),
-		socket: "/fake/path/to/socket.sock",
-		TbConfig: ConfigSettings{
-			Input: DockerConfig{Period: nil, Socket: nil},
-		},
-		dockerClient: nil,
-		events:       nil,
-		eventGenerator: EventGenerator{
-			networkStats:      map[string]map[string]NetworkData{},
-			blkioStats:        map[string]BlkioData{},
-			calculatorFactory: CalculatorFactoryImpl{},
-			period:            time.Second,
-		},
-		minimalDockerVersion: SoftwareVersion{major: 1, minor: 5},
+// DOCKERBEAT TYPE IS CURRENTLY NOT REALLY TESTABLE BECAUSE OF THE DOCKER GO CLIENT WICH DOES NOT DEFINE INTERFACE
+// (NOT MOCKABLE)
+
+// SETUP TESTS
+
+func TestDockerbeatSetupMethod(t *testing.T) {
+	// GIVEN
+	// a dockerbeat instance
+	var dockerbeat = getEmptyDockerbeat()
+	events := publisher.ChanClient{}
+	fakeBeat := beat.Beat{Events:events}
+
+	// WHEN
+	dockerbeat.Setup(&fakeBeat)
+
+	// THEN
+	// events set as dockerbeat.events
+	assert.Equal(t, events, dockerbeat.events)
+	// dockerbeat.done initialized
+	assert.NotNil(t, dockerbeat.done)
+	// dockerClient initialized with given socket
+	assert.NotNil(t, dockerbeat.dockerClient)
+	assert.Equal(t, dockerbeat.socket, dockerbeat.dockerClient.Endpoint())
+	// eventGenerator initialized
+	assert.NotNil(t, dockerbeat.eventGenerator)
+	assert.NotNil(t, dockerbeat.eventGenerator.blkioStats)
+	assert.NotNil(t, dockerbeat.eventGenerator.networkStats)
+	assert.NotNil(t, dockerbeat.eventGenerator.calculatorFactory)
+	assert.Equal(t, dockerbeat.period, dockerbeat.eventGenerator.period)
+
+}
+
+// CLOSE TESTS
+
+func TestDockebeatCloseMethod(t *testing.T) {
+	// GIVEN
+	var dockerbeat = getEmptyDockerbeat()
+
+	// WHEN
+	dockerbeat.Stop()
+
+	// THEN
+	_, ok := <-dockerbeat.done
+	if ok {
+		assert.Fail(t, "dockerbeat.done not closed")
 	}
 }
+
+// VALID VERSION TESTS
 
 func TestDockerbeatValidVersionTooOld(t *testing.T) {
 	// GIVEN
@@ -69,5 +100,26 @@ func TestDockerbeatValidVersion(t *testing.T) {
 		// THEN
 		assert.True(t, valid)
 		assert.Nil(t, err)
+	}
+}
+
+// helper method
+func getEmptyDockerbeat() Dockerbeat {
+	return Dockerbeat{
+		done:   make(chan struct{}),
+		period: time.Duration(10),
+		socket: "/fake/path/to/socket.sock",
+		TbConfig: ConfigSettings{
+			Input: DockerConfig{Period: nil, Socket: nil},
+		},
+		dockerClient: nil,
+		events:       nil,
+		eventGenerator: EventGenerator{
+			networkStats:      map[string]map[string]NetworkData{},
+			blkioStats:        map[string]BlkioData{},
+			calculatorFactory: CalculatorFactoryImpl{},
+			period:            time.Second,
+		},
+		minimalDockerVersion: SoftwareVersion{major: 1, minor: 5},
 	}
 }
