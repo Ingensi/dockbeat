3221ac726613bf74bc1420b340d7f66e29d592df
Thibaut Marmin <marminthibaut@gmail.com>
2016-03-15 15:07:11 +0100

    gofmt

--- beat/Dockerbeat.go
+++ beat/Dockerbeat.go
@@ -6,20 +6,20 @@ import (
 	"strings"
 	"time"
 
+	"fmt"
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/cfgfile"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
 	"github.com/fsouza/go-dockerclient"
-	"fmt"
 )
 
 // const for event logs
 const (
 	ERROR = "error"
-	WARN = "warning"
-	INFO = "info"
+	WARN  = "warning"
+	INFO  = "info"
 	DEBUG = "debug"
 	TRACE = "trace"
 )
@@ -210,8 +210,8 @@ func (d *Dockerbeat) checkPrerequisites() error {
 
 		if !valid {
 			output = errors.New("Docker server is too old (version " +
-			strconv.Itoa(d.minimalDockerVersion.major) + "." + strconv.Itoa(d.minimalDockerVersion.minor) + ".x" +
-			" and earlier is required)")
+				strconv.Itoa(d.minimalDockerVersion.major) + "." + strconv.Itoa(d.minimalDockerVersion.minor) + ".x" +
+				" and earlier is required)")
 		}
 
 	} else {
@@ -240,7 +240,7 @@ func (d *Dockerbeat) validVersion(version string) (bool, error) {
 	var output bool
 
 	if actualMajorVersion > d.minimalDockerVersion.major ||
-	(actualMajorVersion == d.minimalDockerVersion.major && actualMinorVersion >= d.minimalDockerVersion.minor) {
+		(actualMajorVersion == d.minimalDockerVersion.major && actualMinorVersion >= d.minimalDockerVersion.minor) {
 		output = true
 	} else {
 		output = false
