5b8977d096f4e9efd1e573782b51f8f08a54fa66
Thibaut Marmin <marminthibaut@gmail.com>
2016-04-15 15:59:02 +0200

    add mocks

---
+++ calculator/mocks/blkio.go
@@ -0,0 +1,44 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+type BlkioCalculator struct {
+	mock.Mock
+}
+
+func (_m *BlkioCalculator) GetReadPs() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
+func (_m *BlkioCalculator) GetWritePs() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
+func (_m *BlkioCalculator) GetTotalPs() float64 {
+	ret := _m.Called()
+
+	var r0 float64
+	if rf, ok := ret.Get(0).(func() float64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(float64)
+	}
+
+	return r0
+}
