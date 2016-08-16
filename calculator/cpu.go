2cd7a63d63228ce8d7ce76185ebae33f310b6ac3
Thibaut Marmin <marminthibaut@users.noreply.github.com>
2016-08-16 14:14:23 +0200

    resolve massive CPU metrics (#116)
    closes #112

--- calculator/cpu.go
+++ calculator/cpu.go
@@ -29,28 +29,33 @@ func (c CPUCalculatorImpl) PerCpuUsage() common.MapStr {
 	if cap(c.New.PerCpuUsage) == cap(c.Old.PerCpuUsage) {
 		output = common.MapStr{}
 		for index := range c.New.PerCpuUsage {
-			output["cpu"+strconv.Itoa(index)] = c.calculateLoad(c.New.PerCpuUsage[index] - c.Old.PerCpuUsage[index])
+			output["cpu"+strconv.Itoa(index)] = c.calculateLoad(c.New.PerCpuUsage[index], c.Old.PerCpuUsage[index])
 		}
 	}
 	return output
 }
 
 func (c CPUCalculatorImpl) TotalUsage() float64 {
-	return c.calculateLoad(c.New.TotalUsage - c.Old.TotalUsage)
+	return c.calculateLoad(c.New.TotalUsage, c.Old.TotalUsage)
 }
 
 func (c CPUCalculatorImpl) UsageInKernelmode() float64 {
-	return c.calculateLoad(c.New.UsageInKernelmode - c.Old.UsageInKernelmode)
+	return c.calculateLoad(c.New.UsageInKernelmode, c.Old.UsageInKernelmode)
 }
 
 func (c CPUCalculatorImpl) UsageInUsermode() float64 {
-	return c.calculateLoad(c.New.UsageInUsermode - c.Old.UsageInUsermode)
+	return c.calculateLoad(c.New.UsageInUsermode, c.Old.UsageInUsermode)
 }
 
-func (c CPUCalculatorImpl) calculateLoad(value uint64) float64 {
+func (c CPUCalculatorImpl) calculateLoad(oldValue uint64, newValue uint64) float64 {
 	// value is the count of CPU nanosecond in 1sec
 	// TODO save the old stat timestamp and reuse here in case of docker read time changes...
 	// 1s = 1000000000 ns
 	// value / 1000000000
+
+	value := int64(oldValue - newValue)
+	if value < 0 {
+		value = 0
+	}
 	return float64(value) / float64(1000000000)
 }
